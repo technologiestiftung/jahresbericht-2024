c67e266306e6e8843f2695bdde9d12a5379d2094
Luise Brandenburger <luise.brandenburger@ts.berlin>
2024-11-13 15:24:34 +0100

    feat: add img optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
@@ -14,6 +15,24 @@ export default defineConfig({
         titleProp: true,
       },
       include: "**/*.svg",
+    }),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 100,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100,
+      },
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true,
+      },
     }),
   ],
 });
