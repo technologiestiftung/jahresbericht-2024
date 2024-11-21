78428297dcfa88722712993b44602e8d25fa1437
Jakob Sawal <jakob.sawal@ts.berlin>
2024-11-21 15:26:02 +0100

    fix: Sticky Section final fixes

--- src/sections/Sticky/index.jsx
+++ src/sections/Sticky/index.jsx
@@ -4,19 +4,10 @@ import cn from "./Sticky.module.scss";
 
 function Sticky({ content, title, id }) {
   const sectionRef = useRef(null);
-  const fixedMarginBottom = "100vh";
-  const halfMarginBottom = "50vh";
   const firstContainerRef = useRef(null);
   const secondContainerRef = useRef(null);
   const thirdContainerRef = useRef(null);
   const [visible, setVisible] = useState("first");
-  const topPadding = 160;
-  const mobilePadding = 1;
-  const [margins, setMargins] = useState({
-    first: 0,
-    second: 0,
-    third: 0,
-  });
 
   const RenderIMG = () => {
     const setIndex = visible === "first" ? 0 : visible === "second" ? 1 : 2;
@@ -25,8 +16,7 @@ function Sticky({ content, title, id }) {
         <div
           className={cn.bg}
           style={{
-            backgroundImage: `url('${content[setIndex].img.src}')`,
-            backgroundSize: title === "Prototyping" ? "contain" : "cover",
+            backgroundImage: `url('${content[setIndex].img.mobileSrc || content[setIndex].img.src}')`,
           }}
         />
       );
@@ -48,15 +38,9 @@ function Sticky({ content, title, id }) {
       const first = firstContainerRef?.current?.getBoundingClientRect();
       const second = secondContainerRef?.current?.getBoundingClientRect();
       const third = thirdContainerRef?.current?.getBoundingClientRect();
-      if (window.innerWidth < 1280) {
-        if (first?.bottom >= 0) return setVisible("first");
-        if (first?.bottom < 0 && second.top >= 0) return setVisible("second");
-        if (third && second?.bottom < 0) return setVisible("third");
-        return;
-      }
-      if (first.top >= 0) return setVisible("first");
-      if (first.top < 0 && second.top >= 0) return setVisible("second");
-      if (third && second.top < 0) return setVisible("third");
+      if (first?.bottom >= 0) return setVisible("first");
+      if (first?.bottom < 0 && second.top >= 0) return setVisible("second");
+      if (third && second?.bottom < 0) return setVisible("third");
     };
     window.addEventListener("scroll", handleScroll);
     handleScroll();
@@ -65,87 +49,16 @@ function Sticky({ content, title, id }) {
     };
   }, [content?.length]);
 
-  useEffect(() => {
-    const getMargins = {
-      ...margins,
-    };
-    if (thirdContainerRef?.current) {
-      margins.third =
-        window.innerHeight -
-        thirdContainerRef?.current?.getBoundingClientRect().height -
-        topPadding;
-    }
-    if (secondContainerRef?.current) {
-      margins.second =
-        window.innerHeight -
-        secondContainerRef?.current?.getBoundingClientRect().height -
-        topPadding;
-    }
-    if (firstContainerRef?.current) {
-      margins.first =
-        window.innerHeight -
-        firstContainerRef?.current?.getBoundingClientRect().height -
-        topPadding;
-    }
-    setMargins(getMargins);
-  }, []);
-
   if (!content) return <></>;
 
   return (
     <section className={cn.wrapper} ref={sectionRef} id={`${id}-sticky`}>
       <RenderIMG />
-      <div
-        className={cn.content}
-        style={
-          window.innerWidth >= 1280
-            ? {
-                paddingBottom: margins.third || margins.second || margins.first,
-              }
-            : { paddingBottom: window.innerHeight * mobilePadding * 0.5 }
-        }
-      >
+      <div className={cn.content}>
         {content.map((current, index) => (
           <div
             key={current.id}
-            style={
-              fixedMarginBottom
-                ? {
-                    marginBottom:
-                      index + 1 === content.length
-                        ? halfMarginBottom
-                        : fixedMarginBottom,
-                  }
-                : window.innerWidth >= 1280
-                  ? {
-                      marginBottom:
-                        content?.length === 3
-                          ? !index
-                            ? margins.first
-                            : index === 1
-                              ? margins.second
-                              : 0
-                          : content?.length === 2
-                            ? !index
-                              ? margins.first
-                              : 0
-                            : 0,
-                    }
-                  : {
-                      marginBottom:
-                        content?.length === 3
-                          ? !index
-                            ? window.innerHeight * mobilePadding
-                            : index === 1
-                              ? window.innerHeight * mobilePadding
-                              : 0
-                          : content?.length === 2
-                            ? !index
-                              ? window.innerHeight * mobilePadding
-                              : 0
-                            : 0,
-                    }
-            }
+            style={{ marginBottom: index + 1 === content.length ? 0 : "100vh" }}
           >
             <div
               className={cn.container}
